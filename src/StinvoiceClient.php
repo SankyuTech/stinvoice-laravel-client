ef2035b4e01c5e1391fcfda747197e53099df862
warfee <warfee619916@gmail.com>
2024-11-13 10:23:51 +0800

    remove unnecessary  table and actions

--- src/StinvoiceClient.php
+++ src/StinvoiceClient.php
@@ -2,20 +2,7 @@
 
 namespace Sankyutech\StInvoiceClient;
 
-use Sankyutech\StInvoiceClient\Class\CompanySubmission;
-use Sankyutech\StInvoiceClient\Class\Invoices;
-use Sankyutech\StInvoiceClient\Class\InvoicesTaxes;
-use Sankyutech\StInvoiceClient\Class\InvoiceAllowanceCharges;
-use Sankyutech\StInvoiceClient\Class\InvoiceItems;
-use Sankyutech\StInvoiceClient\Class\InvoiceItemTaxes;
-use Sankyutech\StInvoiceClient\Class\InvoiceItemAllowanceCharges;
-use Sankyutech\StInvoiceClient\Class\InvoiceItemCommodity;
-
-
 class StinvoiceClient
 {
-	public function createRecord($datas){
 
-		// insert to database for recording on submission 
-	}
 }
